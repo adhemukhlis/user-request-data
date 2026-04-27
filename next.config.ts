fa22fbe2e13472f8136af63e4d30e1c80ace9f3e
adhemukhlis <adhemukhlis@gmail.com>
2026-04-27 16:19:29 +0700

    add CORS (*)

--- next.config.ts
+++ next.config.ts
@@ -25,12 +25,26 @@ const nextConfig: NextConfig = {
 		resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.mjs', '.json'],
 	},
 	typedRoutes: true,
-	experimental: {
-		serverSourceMaps: false,
-		serverActions: {
-			bodySizeLimit: '5mb',
-		},
-		authInterrupts: true,
+	async headers() {
+		return [
+			{
+				source: '/api/:path*',
+				headers: [
+					{
+						key: 'Access-Control-Allow-Origin',
+						value: '*',
+					},
+					{
+						key: 'Access-Control-Allow-Methods',
+						value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+					},
+					{
+						key: 'Access-Control-Allow-Headers',
+						value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+					},
+				],
+			},
+		]
 	},
 }
 
